3105d91925395bb8ef375789bdeacba82b0a25ab
cbteeple <cbteeple@g.harvard.edu>
2020-01-18 18:39:35 -0500

    Images are only scrolled to if they are not on the screen

--- assets/js/img-modal.js
+++ assets/js/img-modal.js
@@ -1,3 +1,40 @@
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+
+
+
+
+
+
+
 var modalOverlay = document.getElementById("modal-overlay");
 var modalClose = document.getElementById("modal-close");
 var modalNext = document.getElementById("modal-next");
@@ -18,7 +55,16 @@ var openModal = function (img) {
     // img.classList.add('active-modal');
     imageClicked = img;
     modalImg.src = img.src;
-    modalImgLink.href= "#"+img.id;
+
+    if ( $(img).visible(true) ){
+        modalImgLink.href= "javascript:void(0);"
+    }
+    else{
+        modalImgLink.href= "#"+img.id;
+    }
+
+
+    
     modalDes.innerHTML = img.alt;
     modalOverlay.classList.add('active');
     setButtons(img);
@@ -167,3 +213,6 @@ document.addEventListener('keyup', function (e) {
     }
 }
 )
+
+
+
