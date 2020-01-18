ee743e4125c33243e80d47987fde28bceb127043
cbteeple <cbteeple@g.harvard.edu>
2020-01-18 14:18:03 -0500

    all external links can be set to open in a new tab

---
+++ assets/js/ext-links.js
@@ -0,0 +1,9 @@
+// any link that is not part of the current domain is modified
+
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   } 
+}
